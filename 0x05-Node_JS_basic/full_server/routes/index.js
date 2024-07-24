25373c1f17c88c5ac2a8affaf20e97b816071a0d
kidusshoa <captainkevin2008@gmail.com>
2024-07-24 11:32:06 +0300

    minor change

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,11 +1,12 @@
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const mapRoutes = (app) => {
+const routes = (app) => {
   app.get('/', AppController.getHomepage);
   app.get('/students', StudentsController.getAllStudents);
   app.get('/students/:major', StudentsController.getAllStudentsByMajor);
 };
 
-export default mapRoutes;
-module.exports = mapRoutes;
+export default routes;
+
+module.exports = routes;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import mapRoutes from './routes';
+import routes from './routes';
 
 const app = express();
 const PORT = 1245;
 
-mapRoutes(app);
+routes(app);
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
